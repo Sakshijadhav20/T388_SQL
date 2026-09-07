ef2326c255239663e09ed632b6528421f151cd77
Sakshi Jadhav <sakshijad937@gmail.com>
2026-09-07 11:09:39 +0530

    sql file uploaded

---
+++ T_388_SQL.sql
@@ -0,0 +1,64 @@
+create database T388;
+use T388;
+show databases;
+
+CREATE TABLE Employee (
+  EmployeeId INT PRIMARY KEY,
+  FullName VARCHAR(45) NOT NULL,
+  Department VARCHAR(45) NOT NULL,
+  Salary float NOT NULL,
+  Gender VARCHAR(45) NOT NULL,
+  Age INT NOT NULL);
+
+select * from employee;
+delete from employee;
+
+insert into Employee values 
+(2005," Sakshi Jadhav", "IT", 50000, "Female", 24);
+
+delete from employee;
+
+INSERT INTO Employee values
+(1001,"John Doe","IT",35000,"Male",25), 
+(1002, 'Mary Smith', 'HR', 45000, 'Female', 27), 
+(1003, 'James Brown', 'Finance', 50000, 'Male', 28), 
+(1004, 'Mike Walker', 'Finance', 50000, 'Male', 28),
+(1005, 'Linda Jones', 'HR', 75000, 'Female', 26), 
+(1006, 'Anurag Mohanty', 'IT', 35000, 'Male', 25), 
+(1007, 'Priyanka Dewangan', 'HR', 45000, 'Female', 27), 
+(1008, 'Sambit Mohanty', 'IT', 50000, 'Male', 28), 
+(1009, 'Pranaya Kumar', 'IT', 50000, 'Male', 28), 
+(1010, 'Hina Sharma', 'HR', 75000, 'Female', 26);
+
+select * from employee;
+delete from employee where Gender="Male";
+delete from employee where Age>=25;
+INSERT INTO Employee values
+(1001,"John Doe","IT",35000,"Male",25), 
+(1002, 'Mary Smith', 'HR', 45000, 'Female', 27), 
+(1003, 'James Brown', 'Finance', 50000, 'Male', 28), 
+(1004, 'Mike Walker', 'Finance', 50000, 'Male', 28),
+(1005, 'Linda Jones', 'HR', 75000, 'Female', 26), 
+(1006, 'Anurag Mohanty', 'IT', 35000, 'Male', 25), 
+(1007, 'Priyanka Dewangan', 'HR', 45000, 'Female', 27), 
+(1008, 'Sambit Mohanty', 'IT', 50000, 'Male', 28), 
+(1009, 'Pranaya Kumar', 'IT', 50000, 'Male', 28), 
+(1010, 'Hina Sharma', 'HR', 75000, 'Female', 26);
+
+Truncate Table employee;
+Drop Table employee;
+
+Alter table employee add Location varchar(10);
+Alter table employee add Bonus float after salary;
+Alter table employee add Title varchar(10) first;
+
+DESC employee;
+
+Alter table employee modify FullName  varchar(35);
+Alter table employee change column Location Address varchar(35);
+
+Update employee set address="Thane";
+Update employee set address="Dombivli" where department="IT";
+Update employee set Title="Mrs" where Gender="Female";
+Update employee set Title="Mr" where Gender="male";
+update employee set bonus= salary*0.5;
